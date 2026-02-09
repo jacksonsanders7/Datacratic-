0dbe89b7e91d916f140dcc14ff344d16885a976b
Jackson Sanders <jacksonsanders2019@icloud.com>
2026-02-09 08:11:13 -0500

    Delete backend directory

--- backend/models/user.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, String, Boolean
-from database import Base
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(String, primary_key=True)
-    email = Column(String, unique=True, index=True)
-    is_member = Column(Boolean, default=True)
